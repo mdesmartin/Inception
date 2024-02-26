034dbb3742cf901ddde7d789214de02e844a06c2
mdesmartin <mdesmartin@icloud.com>
2024-02-26 22:33:07 +0100

    clean

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-# PATH_TO_DATA = /home/ebillon/data/
+PATH_TO_DATA = /Users/mdesmartin/data/
 
 all:
 	$(MAKE) up
 
 up:
-	# mkdir -p $(PATH_TO_DATA)wordpress
-	# mkdir -p $(PATH_TO_DATA)mariadb
+	mkdir -p $(PATH_TO_DATA)wordpress
+	mkdir -p $(PATH_TO_DATA)mariadb
 	docker compose -f srcs/docker-compose.yml up -d --build
 
 down:
@@ -15,14 +15,9 @@ clean:
 	docker compose -f srcs/docker-compose.yml down --rmi local -v
 
 fclean:
-	docker compose -f srcs/docker-compose.yml down --rmi all -v --remove-orphans
+	docker compose -f srcs/docker-compose.yml down --rmi all -v \
 	# rm -rf $(PATH_TO_DATA)*
 	
 
 re: fclean
 	$(MAKE) up
-
-
-# docker compose up -d --build
-
-# docker compose down -v
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -2,100 +2,19 @@
 
 cd /var/www/wordpress
 
-# Installer WordPress
+# Install WordPress
 wp core download --path=. --allow-root
 
-# Check if wp-config.php exists, if not, create it
-# if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-    wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root
-# fi
+# Create wp-config.php 
+wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root
 
-# # Installer WordPress
+# Install WordPress
 wp core install --url=${WP_URL} --title="${WP_TITLE}" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
+# Create a second user
 wp user create --allow-root $WP_OTHER_USER $WP_OTHER_MAIL --user_pass=$WP_OTHER_PASSWORD
 
-chown www-data:www-data /var/www/wordpress/wp-content/uploads -R #
+# Give right to the contener to upload files locally
+chown www-data:www-data /var/www/wordpress/wp-content/uploads -R
 
-
-# Démarrer PHP-FPM en mode "foreground"
 php-fpm7.4 -F
-
-
-
-# cd /var/www/wordpress
-
-# if [ ! -f wp-config.php ]; then
-# 	wp core download --path=. --allow-root
-
-# 	wp config create --allow-root 	--dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST
-
-# 	wp db create --allow-root
-
-# 	wp core --allow-root install 	--url=$DOMAIN --title=$WP_TITLE \
-# --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL
-
-# 	wp user create --allow-root $WP_BASIC_USER $WP_BASIC_MAIL --user_pass=$WP_BASIC_PASSWORD
-
-# 	chown www-data:www-data /var/www/wordpress/wp-content/uploads -R
-# fi
-
-# php-fpm7.4 -F
-
-
-
-
-
-
-# #!/bin/bash
-
-# # Afficher les informations de débogage
-# echo "Chemin de wp-cli.phar : $(which wp)"
-# echo "Chemin du modèle wp-config.mustache : /chemin/vers/wp-cli/wp-cli/templates/wp-cli/config-command/templates/wp-config.mustache"
-
-# # Installer WordPress
-# wp core download --path=/var/www/wordpress --allow-root
-
-# # Vérifier et Créer wp-config.php
-# if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-#     echo "Fichier wp-config.php non trouvé, création..."
-#     wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --path=/var/www/wordpress --allow-root
-# else
-#     echo "Fichier wp-config.php déjà existant."
-# fi
-
-# # Afficher le contenu du répertoire pour débogage
-# echo "Contenu de /var/www/wordpress :"
-# ls -la /var/www/wordpress
-
-# # Démarrer PHP-FPM en mode "foreground"
-# php-fpm7.4 -F
-
-
-# #!/bin/bash
-
-# # Afficher les informations de débogage
-# echo "Chemin de wp-cli.phar : $(which wp)"
-# echo "Chemin du modèle wp-config.mustache : /chemin/vers/wp-cli/wp-cli/templates/wp-cli/config-command/templates/wp-config.mustache"
-
-# # Installer WordPress
-# wp core download --path=/var/www/wordpress --allow-root
-
-# # Vérifier et Créer wp-config.php
-# if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
-#     echo "Fichier wp-config.php non trouvé, création..."
-#     wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --path=/var/www/wordpress --allow-root
-# else
-#     echo "Fichier wp-config.php déjà existant."
-# fi
-
-# # Afficher le contenu du répertoire pour débogage
-# echo "Contenu de /var/www/wordpress :"
-# ls -la /var/www/wordpress
-
-# # Vérifier le contenu de wp-config.php
-# echo "Contenu de wp-config.php :"
-# cat /var/www/wordpress/wp-config.php
-
-# # Démarrer PHP-FPM en mode "foreground"
-# php-fpm7.4 -F
